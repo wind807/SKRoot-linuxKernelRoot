390d9625494a52912268e99c9c597422c24d70c1
unkn0wName <unkn0wName@gmail.com>
2025-05-16 20:24:17 +0800

    fix: 优化逻辑

--- patch_kernel_root/patch_filldir64.cpp
+++ patch_kernel_root/patch_filldir64.cpp
@@ -9,56 +9,35 @@ PatchFilldir64::~PatchFilldir64()
 {
 }
 
-int PatchFilldir64::get_atomic_usage_len() {
-	int len = 8;
-	if (m_analyze_kernel.is_kernel_version_less("6.6.0")) {
-		len = 4;
-	}
-	return len;
-}
-
-size_t PatchFilldir64::patch_filldir64(size_t root_key_addr_offset, size_t hook_func_start_addr, const std::vector<size_t>& task_struct_offset_cred, std::vector<patch_bytes_data>& vec_out_patch_bytes_data) {
+size_t PatchFilldir64::patch_filldir64(size_t root_key_addr_offset, size_t hook_func_start_addr, std::vector<patch_bytes_data>& vec_out_patch_bytes_data) {
 	size_t filldir64_addr = m_sym.filldir64;
-	int atomic_usage_len = get_atomic_usage_len();
 
 	int root_key_adr_offset = root_key_addr_offset - (hook_func_start_addr + 4 * 4);
 
 	size_t filldir64_entry_hook_jump_back_addr = filldir64_addr + 4;
-	auto next_asm_line_bytes_cnt = (task_struct_offset_cred.size() - 1) * 4;
 	std::stringstream sstrAsm;
 	sstrAsm
 		<< "CMP W2, #16" << std::endl
-		<< "BNE #" << 92 + next_asm_line_bytes_cnt << std::endl
+		<< "BNE #72" << std::endl
 		<< "STP X7, X8, [sp, #-16]!" << std::endl
 		<< "STP X9, X10, [sp, #-16]!" << std::endl
-		<< "ADR X7, #"<< root_key_adr_offset << std::endl
+		<< "ADR X7, #" << root_key_adr_offset << std::endl
 		<< "MOV X8, #0" << std::endl
 		<< "LDRB W9, [X1, X8]" << std::endl
 		<< "LDRB W10, [X7, X8]" << std::endl
 		<< "CMP W9, W10" << std::endl
-		<< "B.NE #" << 52 + next_asm_line_bytes_cnt << std::endl
+		<< "B.NE #32" << std::endl
 		<< "ADD X8, X8, 1" << std::endl
 		<< "CMP X8, #16" << std::endl
-		<< "BLT #-24" << std::endl;
-		sstrAsm << "MRS X7, SP_EL0" << std::endl;
-		for (auto x = 0; x < task_struct_offset_cred.size(); x++) {
-			if (x != task_struct_offset_cred.size() - 1) {
-				sstrAsm << "LDR X7, [X7, #" << task_struct_offset_cred[x] << "]" << std::endl;
-			}
-		}
-		sstrAsm << "LDR X7, [X7, #" << task_struct_offset_cred[task_struct_offset_cred.size() - 1] << "]" << std::endl
-		<< "CBZ X7, #" << 28 << std::endl
-		<< "ADD X7, X7, #"<< atomic_usage_len << std::endl
-		<< "MOV X8, #8" << std::endl
-		<< "LDR  W9, [X7], #4" << std::endl
-		<< "CBNZ  W9, #" << 88 << std::endl
-		<< "SUBS  X8, X8, #1" << std::endl
-		<< "B.NE #-" << 12 << std::endl
+		<< "BLT #-24" << std::endl
+		<< "LDP X9, X10, [sp], #16" << std::endl
+		<< "LDP X7, X8, [sp], #16" << std::endl
+		<< "MOV X0, XZR" << std::endl
+		<< "RET" << std::endl
 		<< "LDP X9, X10, [sp], #16" << std::endl
 		<< "LDP X7, X8, [sp], #16" << std::endl
 		<< "MOV X0, X0" << std::endl
-		<< "B #" << filldir64_entry_hook_jump_back_addr - (hook_func_start_addr + 0x64 + next_asm_line_bytes_cnt) << std::endl;
-
+		<< "B #" << filldir64_entry_hook_jump_back_addr - (hook_func_start_addr + 0x50) << std::endl;
 	std::string strAsmCode = sstrAsm.str();
 	std::cout << std::endl << strAsmCode << std::endl;
 
@@ -71,7 +50,7 @@ size_t PatchFilldir64::patch_filldir64(size_t root_key_addr_offset, size_t hook_
 	char hookOrigCmd[4] = { 0 };
 	memcpy(&hookOrigCmd, (void*)((size_t)&m_file_buf[0] + filldir64_addr), sizeof(hookOrigCmd));
 	std::string strHookOrigCmd = bytes_2_hex_str((const unsigned char*)hookOrigCmd, sizeof(hookOrigCmd));
-	strBytes = strBytes.substr(0, (0x60 + next_asm_line_bytes_cnt) * 2) + strHookOrigCmd + strBytes.substr((0x60 + next_asm_line_bytes_cnt + 4) * 2);
+	strBytes = strBytes.substr(0, (0x4C) * 2) + strHookOrigCmd + strBytes.substr((0x4C + 4) * 2);
 
 	vec_out_patch_bytes_data.push_back({ strBytes, hook_func_start_addr });
 	std::stringstream sstrAsm2;
--- patch_kernel_root/patch_filldir64.h
+++ patch_kernel_root/patch_filldir64.h
@@ -10,10 +10,9 @@ public:
 		const AnalyzeKernel& analyze_kernel);
 	~PatchFilldir64();
 
-	size_t patch_filldir64(size_t root_key_addr_offset, size_t hook_func_start_addr, const std::vector<size_t>& task_struct_offset_cred, std::vector<patch_bytes_data>& vec_out_patch_bytes_data);
+	size_t patch_filldir64(size_t root_key_addr_offset, size_t hook_func_start_addr, std::vector<patch_bytes_data>& vec_out_patch_bytes_data);
 
 private:
-	int get_atomic_usage_len();
 	const std::vector<char>& m_file_buf;
 	const KernelSymbolOffset& m_sym;
 	const AnalyzeKernel& m_analyze_kernel;
