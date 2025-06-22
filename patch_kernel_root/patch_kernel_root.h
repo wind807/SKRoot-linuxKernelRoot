6942e399d61a0047ff43ea8d88151f9352e5493a
unkn0wName <unkn0wName@gmail.com>
2025-06-22 16:01:48 +0800

    fix: 修复卡重启问题，修复部分6.6无法开机问题

--- patch_kernel_root/patch_kernel_root.h
+++ patch_kernel_root/patch_kernel_root.h
@@ -3,11 +3,11 @@
 #include <filesystem>
 #define IF_EXIT(cond) do { if (cond) { system("pause"); exit(0); } } while (0)
 
-#define APPLY_PATCH(call_expr)                  \
-    do {                                       \
-        shellcode_size = (call_expr);          \
-        IF_EXIT(!shellcode_size);              \
-        next_hook_start_region.consume(shellcode_size); \
+#define PATCH_AND_CONSUME(region, call_expr) \
+    do { \
+        size_t __sz = (call_expr); \
+        IF_EXIT(!__sz); \
+        (region).consume(__sz); \
     } while (0)
 
 struct patch_bytes_data {
@@ -17,15 +17,15 @@ struct patch_bytes_data {
 
 static size_t patch_ret_cmd(const std::vector<char>& file_buf, size_t start, std::vector<patch_bytes_data>& vec_out_patch_bytes_data) {
 	vec_out_patch_bytes_data.push_back({ "C0035FD6", start });
-	return start + 4;
+	return 4;
 }
 
 static size_t patch_ret_1_cmd(const std::vector<char>& file_buf, size_t start, std::vector<patch_bytes_data>& vec_out_patch_bytes_data) {
 	vec_out_patch_bytes_data.push_back({ "200080D2C0035FD6", start });
-	return start + 4 * 2;
+	return 8;
 }
 
 static size_t patch_ret_0_cmd(const std::vector<char>& file_buf, size_t start, std::vector<patch_bytes_data>& vec_out_patch_bytes_data) {
 	vec_out_patch_bytes_data.push_back({ "E0031F2AC0035FD6", start });
-	return start + 4 * 2;
+	return 8;
 }
