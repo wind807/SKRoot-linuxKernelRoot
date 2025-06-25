daf8b049a41791e2713c910136d611bc27180548
unkn0wName <unkn0wName@gmail.com>
2025-06-25 17:48:11 +0800

    rename

--- testRoot/jni/kernel_root_kit/kernel_root_kit_umbrella.h
+++ testRoot/jni/kernel_root_kit/kernel_root_kit_umbrella.h
@@ -1,5 +1,4 @@
-#ifndef KERNEL_ROOT_KIT_UMBRELLA_H_
-#define KERNEL_ROOT_KIT_UMBRELLA_H_
+#pragma once
 #include "kernel_root_kit_command.h"
 #include "kernel_root_kit_exec_process.h"
 #include "kernel_root_kit_err_def.h"
@@ -8,4 +7,3 @@
 #include "kernel_root_kit_process_cmdline.h"
 #include "kernel_root_kit_su_install_helper.h"
 #include "kernel_root_kit_parasite_app.h"
-#endif /* KERNEL_ROOT_KIT_UMBRELLA_H_ */
