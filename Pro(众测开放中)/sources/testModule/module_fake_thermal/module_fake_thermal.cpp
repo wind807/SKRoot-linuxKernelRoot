6f64ed85e9d67508e83aa12797cbf87de3065e38
unkn0wName <unkn0wName@gmail.com>
2026-02-16 20:44:07 +0800

    feat: 更新Pro版本4.2.5 新增大量功能

--- Pro(众测开放中)/sources/testModule/module_fake_thermal/module_fake_thermal.cpp
+++ Pro(众测开放中)/sources/testModule/module_fake_thermal/module_fake_thermal.cpp
@@ -25,7 +25,7 @@ int skroot_module_main(const char* root_key, const char* module_private_dir) {
 // SKRoot 模块名片
 SKROOT_MODULE_NAME("温控解除")
 SKROOT_MODULE_VERSION("1.0.0")
-SKROOT_MODULE_DESC("内核级伪装处理器温度曲线，实现去除温控降频，安全可靠。")
+SKROOT_MODULE_DESC("内核级伪装处理器温度曲线，实现去除温控，安全可靠。")
 SKROOT_MODULE_AUTHOR("SKRoot")
 SKROOT_MODULE_UUID32("kO8hT9tT2fB0hY9hV2bB4eJ6aY2nQ6kL")
 SKROOT_MODULE_UPDATE_JSON("https://abcz316.github.io/SKRoot-linuxKernelRoot/module_fake_thermal/update.json")
